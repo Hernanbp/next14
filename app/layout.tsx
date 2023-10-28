aad62b9550141bcf25f103222fc2e56e306ab3c6
Hernan <her.cw@hotmail.com>
2023-10-28 19:44:14 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import "@/app/ui/global.css"
+import { GeistSans } from "geist/font"
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.className} antialiased`}>
       <body>{children}</body>
     </html>
-  );
+  )
 }
